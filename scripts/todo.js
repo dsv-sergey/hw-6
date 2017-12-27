a2a9e17253f9a3703c7ad8e39e234481e6cde707
dsv-sergey <dsv-sergey@mail.ru>
2017-12-28 02:49:55 +0300

    js

--- scripts/todo.js
+++ scripts/todo.js
@@ -13,6 +13,7 @@ function add() {
     var todos = getList() }
     todos.push(addTodo);
     localStorage.setItem('todo', JSON.stringify(todos));
+    document.getElementById('addCase').value = '';
     show();
     return false;
 }
@@ -20,22 +21,36 @@ function show() {
     var todos = getList();
     var todoStr = '<ul>';
     for (var i = 0; i < todos.length; i++) {
-        todoStr += '<li>' + '<input class="check" type="checkbox"/>' + todos[i] + '</li>'
+        todoStr += '<li>' + '<input class="check_line" type="checkbox"  onclick=/>' + todos[i] + '</li>'
     }
     todoStr += '</ul>';
+
     document.getElementById('todo').innerHTML = todoStr;
-    /*
-    var buttons = document.getElementsByClassName('check');
-    for (var i=0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', removeCase);
+
 }
-function removeCase() {
-*/
+function removeSelect(){
+    var rem = [].filter.call(document.querySelectorAll('ul > li > input'),function (element) {
+        return element;
+    });
+    for (var i = 0; i < rem.length; i++) {
+        if (this.children.childNodes["i"].childNodes["i"].value == 'on') rem.parentNode.removeChild(rem);
+    }
+}
+function deleteSel() {
+    var del = document.querySelectorAll('ul > li');
+    for (var i = 0; i < del.length; i++) {
+        if (this.children[i] == 'checked') {
+            this.value = '';
+        }
+    }
+
 }
 function removeAll() {
     localStorage.clear('todo');
     show();
 }
+document.getElementById('removeSelect').addEventListener('click', deleteSel)
+document.getElementById('todo').addEventListener('click', removeSelect);
 document.getElementById('removeAll').addEventListener('click', removeAll);
 document.getElementById('add').addEventListener('click', add);
 show();
